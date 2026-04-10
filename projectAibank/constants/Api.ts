9d942543917025f6d646b2751f99140dcd53057f
John Guaman <johnguaman2003@gmail.com>
2026-04-09 21:32:39 -0500

    Base fr3

---
+++ projectAibank/constants/Api.ts
@@ -0,0 +1,8 @@
+// constants/Api.js
+export const API_BASE_URL = "https://ai-bank-backend-o83m.onrender.com";
+
+export const ENDPOINTS = {
+  segmentar: `${API_BASE_URL}/segmentacion/segmentar`,
+  chat: `${API_BASE_URL}/agent/chat`,
+  health: `${API_BASE_URL}/health`,
+};
